01dd39149170970dd60b03964386a1485df901a6
Henrique Cordeiro <99266503+KikaoCordeiro@users.noreply.github.com>
2022-06-01 11:42:48 -0300

    Update Modulo.py

--- Modulo.py
+++ Modulo.py
@@ -1,6 +1,8 @@
+#Imports
 import csv
 import os
 
+#Variáveis Globais
 global w_main
 global logged
 global w_escolha_2
@@ -18,6 +20,7 @@ global w_mes
 global w_goals
 global nome
 
+#Valores das variáveis booleanas   
 logged = False
 w_main = True
 w_escolha_2 = True
@@ -33,6 +36,7 @@ w_qs = True
 w_mes = True
 w_goals = True
 
+#Listas e Dicionários
 contas_adm = {}
 dados_cadastro = []
 cadastro = []
@@ -43,22 +47,29 @@ senhas = []
 cpfs = []
 nomes = []
 
+#Limpeza do terminal
 def limpa():
     os.system('cls')
 
-def escreverDados(nome_arquivo, nome_lista):
+#Escrita dos dados nos arquivos CSV
+def escrever_dados(nome_arquivo, nome_lista):
     with open(nome_arquivo, 'a') as adm:
         esc = ','.join(nome_lista)
         adm.writelines(esc + '\n')
 
+#Login do usuário na plataforma
 def login():
-    limpa()
     global user
+
+    limpa()
+    
     user = input('Digite o email de login: ')
     senha = input('Digite sua senha: ')
+
     with open('DadosContas.csv','r') as adm:
         leitor = csv.DictReader(adm)
         contas_adm = {l['colEmail']:l['colSenha'] for l in leitor}
+
     if user not in contas_adm:
         return 'cadastro'
     elif user in contas_adm and contas_adm[user]!= senha:
@@ -69,14 +80,21 @@ def login():
         print('Não foi possível fazer o login') 
         pass
 
+#Cadastro do usuário
 def Cadastro():
     global nome
-    wSenha = True
+
+    w_senha = True
     w_cadastro_mod = True
+
     existence()
+
     while w_cadastro_mod:
+
         limpa()
+
         user = input('Digite seu email de Login: ')
+
         if user not in emails:        
             nome = input('Digite seu nome: ')
             cpf = input('Digite seu cpf: ')
@@ -84,14 +102,14 @@ def Cadastro():
                 dados_cadastro.append(user)
                 dados_cadastro.append(nome)
                 dados_cadastro.append(cpf)
-                while wSenha:
-                    if passConfirm(dados_cadastro) == True: 
+                while w_senha:
+                    if pass_confirm(dados_cadastro) == True: 
                         limpa()
-                        escreverDados('DadosContas.csv',dados_cadastro)
-                        wSenha = False
+                        escrever_dados('DadosContas.csv',dados_cadastro)
+                        w_senha = False
                         return True  
-                    elif passConfirm(dados_cadastro) == False:
-                        wSenha = False
+                    elif pass_confirm(dados_cadastro) == False:
+                        w_senha = False
                         w_cadastro_mod = False
                         return 'senhaNaoCoincide'
             else:
@@ -100,20 +118,24 @@ def Cadastro():
         else:
             w_cadastro_mod = False
             return 'jaCadastrado'
-
+#Cadastro/Login do voluntário de ronda
 def volunt_reg(logged):
     global nome
-    w_reg = True
-    wSenha = True
     global user
     global senha
+
+    w_reg = True
+    w_senha = True
+
     limpa()
     existence()
+
     while w_reg:
         if logged == True:
             cpf = cpfs[emails.index(user)]
             senha = senhas[emails.index(user)]
             nome = nomes[emails.index(user)]
+
             dados_ronda.append(input('Insira seu número de telefone: '))
             dados_ronda.append(input('Insira o nome do seu contato de emergência: '))
             dados_ronda.append(input('Insira o número do seu contato de emergência: '))
@@ -121,11 +143,14 @@ def volunt_reg(logged):
             dados_ronda.insert(1,nome)
             dados_ronda.insert(2,cpf)
             dados_ronda.insert(3,senha)
-            escreverDados('DadosSGRonda.csv',dados_ronda)
+
+            escrever_dados('DadosSGRonda.csv',dados_ronda)
             w_reg = False
             return True
+
         if logged == False:
             log = int(input('[1] - Login [2] - Cadastro\n'))
+
             if log == 1:
                 limpa()
                 user = input('Insira o email para login: ')
@@ -152,8 +177,8 @@ def volunt_reg(logged):
                     return 'cpf_usado'
                 else:
                     nome = input('Insira seu nome: ')
-                    while wSenha:
-                        if passConfirm(dados_ronda) == True: 
+                    while w_senha:
+                        if pass_confirm(dados_ronda) == True: 
                             limpa()
                             senha = senha
                             dados_ronda.append(input('Insira seu número de telefone: '))
@@ -161,34 +186,42 @@ def volunt_reg(logged):
                             dados_ronda.append(input('Insira o número do seu contato de emergência: '))
                             dados_ronda.insert(0, user)
                             dados_ronda.insert(1, nome)
-                            escreverDados('DadosSGRonda.csv', dados_ronda)
+
+                            escrever_dados('DadosSGRonda.csv', dados_ronda)
+
                             dados_cadastro.insert(0,user)
                             dados_cadastro.insert(1,nome)
                             dados_cadastro.insert(2,cpf)
                             dados_cadastro.insert(3,senha)
-                            escreverDados('DadosContas.csv', dados_cadastro)
+
+                            escrever_dados('DadosContas.csv', dados_cadastro)
                             w_reg = False
                             logged = True
-                            wSenha = False
+                            w_senha = False
                             return True  
-                        elif passConfirm(dados_ronda) == False:
-                            wSenha = False
+                        elif pass_confirm(dados_ronda) == False:
+                            w_senha = False
                             w_reg = False
                             return 'senhaNaoCoincide'
                         
-                    
+#Cadastro/Login do voluntário especializado                    
 def volunt_espec(logged):
     global nome
-    w_espec = True
     global user
     global senha
+
+    w_espec = True
+
     limpa()
     existence()
+
     while w_espec:
+
         if logged == True:
             cpf = cpfs[emails.index(user)]
             senha = senhas[emails.index(user)]
             nome = nomes[emails.index(user)]
+
             dados_espec.append(input('Insira seu número de telefone: '))
             dados_espec.append(input('Insira o seu grau de escolaridade: '))
             dados_espec.append(input('Insira sua área de interesse: '))
@@ -197,14 +230,19 @@ def volunt_espec(logged):
             dados_espec.insert(1,nome)
             dados_espec.insert(2,cpf)
             dados_espec.insert(3,senha)
-            escreverDados('DadosSGEspec.csv',dados_espec)
+
+            escrever_dados('DadosSGEspec.csv',dados_espec)
             w_espec = False
             return True
+
         elif logged == False:
+
             log = int(input('[1] - Login [2] - Cadastro\n'))
+
             if log == 1:
                 limpa()
                 user = input('Insira o email para login: ')
+
                 if user not in emails:
                     print('Usuário não encontrado.')
                     proceed = input('Pressione enter para continuar.')
@@ -220,8 +258,8 @@ def volunt_espec(logged):
                         pass
             else:
                 nome = input('Insira seu nome: ')
-                while wSenha:
-                    if passConfirm(dados_espec) == True: 
+                while w_senha:
+                    if pass_confirm(dados_espec) == True: 
                         limpa()
                         senha = senha
                         dados_espec.append(input('Insira seu número de telefone: '))
@@ -230,22 +268,22 @@ def volunt_espec(logged):
                         dados_espec.append(input('Alguma proposta para a organização?\n'))
                         dados_espec.insert(0, user)
                         dados_espec.insert(1, nome)
-                        escreverDados('DadosSGEspec.csv', dados_espec)
+                        escrever_dados('DadosSGEspec.csv', dados_espec)
                         dados_cadastro.insert(0,user)
                         dados_cadastro.insert(1,nome)
                         dados_cadastro.insert(2,cpf)
                         dados_cadastro.insert(3,senha)
-                        escreverDados('DadosContas.csv', dados_cadastro)
+                        escrever_dados('DadosContas.csv', dados_cadastro)
                         w_espec = False
                         logged = True
-                        wSenha = False
+                        w_senha = False
                         return True  
-                    elif passConfirm(dados_espec) == False:
-                        wSenha = False
+                    elif pass_confirm(dados_espec) == False:
+                        w_senha = False
                         w_espec = False
                         return 'senhaNaoCoincide'
-
-def passConfirm(lista):
+#Confirmação da senha do cadastro
+def pass_confirm(lista):
     global senha
     senha = input('Digite sua senha:')
     confsenha = input('Confirme a senha: ')
@@ -256,6 +294,8 @@ def passConfirm(lista):
         limpa()
         return False
 
+#Separação das colunas dos arquivos CSV
+#Serve para facilitar a busca por informações
 def existence():
     with open('DadosContas.csv') as contas:
         leitor = csv.DictReader(contas)
@@ -265,6 +305,7 @@ def existence():
             nomes.append(c['colNome'])
             cpfs.append(c['colCpf'])
 
+#Cadastro/Login do doador mensal
 def donate_mes():
     global logged
     w_mes = True
@@ -284,11 +325,3 @@ def donate_mes():
                     return 'cadastrado'
                 else:
                     return Cadastro()
-
-# def name():
-#     global nome
-#     global user
-#     existence()
-#     nome = nomes[emails.index(user)]
-#     return nome
-# print(name())
